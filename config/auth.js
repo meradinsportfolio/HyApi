7f0ced7e5b2a06d1f23e3ee7c438b8cbf5df4619
Michael Schouten <manschouten@hotmail.com>
2016-07-20 00:36:51 +0200

    Main project layout done mlab connection added

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '208086766252184',
+		'clientSecret' : '087bc351cb71155633a5b8be7c799668',
+		'callbackURL' : 'http://mobdevelopment.herokuapp.com/auth/facebook/callback'
+	},
+	'twitterAuth' : {
+		'consumerKey' : 'TuNpHucgOBqQrQlzSIeqyf4e6',
+		'consumerSecret': 'InKifQgJmq0Fp8Ctinw2KLoLdo9tJVoOYPxW6lk3qpvCrleoEm',
+		'callbackURL' : 'http://mobdevelopment.herokuapp.com/auth/twitter/callback'
+	},
+	'googleAuth' : {
+		'clientID' : '73413103701-5jpfk2hcodo193s5e8mql1uq60td92ln.apps.googleusercontent.com',
+		'clientSecret' : 'UU22db0ptPGOWNegpQL8K7cG',
+		'callbackURL' : 'http://mobdevelopment.herokuapp.com/auth/google/callback'
+	}
+};
