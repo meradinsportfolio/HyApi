acf8010d66a82b9c3edf59728e48ed2bc0922417
Michael Schouten <manschouten@hotmail.com>
2016-08-03 15:17:19 +0200

    change in google oauth route to test

--- routes/user.js
+++ routes/user.js
@@ -111,8 +111,10 @@ router.get('/logout', isLoggedIn, function (req, res) {
 	// GOOGLE ROUTES =======================
 	// =====================================
 		// send to google to do the authentication
-		router.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-		
+		// router.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+		// need to check if this works
+		router.get('/auth/google', passport.authenticate('google', { scope : ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'] }));
+
 		// the callback after google has authenticated the user
 		router.get('/auth/google/callback', passport.authenticate('google', {
 			successRedirect 	: '/user/profile',
